ab5e991a15ebaf64f575ca428da41ae0171a8833
Chukwuebuka <ebuzuaniugo@yahoo.com>
2026-05-28 19:30:33 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 import MaintenancePage from "./FixPage.tsx";
 
-// // Lagos timezone = UTC+1
-// const maintenanceStartDate = new Date("2026-05-21T00:00:00+01:00");
-
-// const isMaintenanceTime = Date.now() >= maintenanceStartDate.getTime();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<App/>
+    <MaintenancePage />
   </StrictMode>,
 );
-// {isMaintenanceTime ? <MaintenancePage /> : <App />}
+
