7a0520870efc6f12b1fbcb832169ef9b9098bc97
Chukwuebuka <ebuzuaniugo@yahoo.com>
2026-07-07 20:57:15 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import MaintenancePage from "./FixPage.tsx";
-
+const allowedCodes = [1990, 1996, 1865, 1864];
+const question = prompt("Enter the code to access the application:");
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MaintenancePage />
+    {allowedCodes.includes(Number(question)) ? <App /> : <MaintenancePage />}
   </StrictMode>,
 );
-
