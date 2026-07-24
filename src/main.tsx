fbb425b5e3e0034bcdb0b383a835637b8f72e15a
chukwuebuka <ebuzuaniugo@yahoo.com>
2026-07-24 23:25:09 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ClerkProvider,SignIn,SignedOut,SignedIn } from "@clerk/clerk-react";
+import { ClerkProvider} from "@clerk/clerk-react";
 import "./index.css";
+import Home from "./Clerk.tsx";
 import App from "./App.tsx";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <SignedOut>
-        <SignIn />
-      </SignedOut>
-
-      <SignedIn>
-        <App />
-      </SignedIn>
+  <Home/>
     </ClerkProvider>
   </StrictMode>
 );
